0576e1959dd01a719690f3ac75168656badc53b3
Stephen Bland <stebland1@gmail.com>
2026-03-13 12:31:36 +0700

    chore: add godotenv for local env vars

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,13 +4,19 @@ import (
 	"log/slog"
 	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/stebland1/live-comments/internal/config"
 	httpapi "github.com/stebland1/live-comments/internal/transport/http"
 )
 
 func main() {
-	cfg := config.Load()
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	if err := godotenv.Load(); err != nil {
+		logger.Info("no environment variable file", "err", err)
+	}
+
+	cfg := config.Load()
 	server := httpapi.NewServer(cfg)
 
 	logger.Info("starting server", "host", cfg.Server.Host, "port", cfg.Server.Port)
