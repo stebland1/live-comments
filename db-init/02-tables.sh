d6fbd68ca31f6b02de44be6565047c370c75e53e
Stephen Bland <stebland1@gmail.com>
2026-03-14 14:50:31 +0700

    update init comments db schema

--- db-init/02-tables.sh
+++ db-init/02-tables.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 psql -v ON_ERROR_STOP=1 -U "$POSTGRES_APP_USER" -d "$POSTGRES_DB" <<-EOSQL
 	CREATE TABLE comments (
 		id bigint GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-		content TEXT
+		user_id bigint NOT NULL,
+		content TEXT NOT NULL
 	);
 EOSQL
